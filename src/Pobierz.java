648c257ac8a014346cf3362e518a46926d6b14b5
camperik94 <tomaszjach94@gmail.com>
2017-12-28 19:59:11 +0100

    Final

--- src/Pobierz.java
+++ src/Pobierz.java
@@ -1,4 +1,20 @@
 
 public class Pobierz {
+	private int lic;
 	
+	public int getLic() {
+		return lic;
+	}
+
+	public void setLic(int lic) {
+		this.lic = lic;
+	}
+
+	int suma = 0;
+
+	public int funkcja(int liczba) {
+		lic = lic + liczba;
+		return lic;
+
+	}
 }
--- src/Zadanie11.java
+++ src/Zadanie11.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 
 public class Zadanie11 {
 	public static void main(String[] args) {
+		Pobierz pob = new Pobierz();
 		
+		Scanner in = new Scanner(System.in);
+		
+		int liczba = 1;
+		
+		
+		
+		while(liczba != 0){
+			liczba = in.nextInt();
+			pob.funkcja(liczba);
+		}
+		System.out.println(pob.getLic());
 	}
 }
